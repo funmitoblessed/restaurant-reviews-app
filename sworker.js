1400272d56519fefefaa05688bec3a170d55fb30
Funmito <funmito.odefemi@outlook.com>
2018-10-26 01:44:53 +0100

    feat: update service worker to cache files

--- sworker.js
+++ sworker.js
@@ -1,8 +1,24 @@
 const cacheThese = [
     '/',
     'index.html',
-    'restaurant.html'
-
+    'restaurant.html',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
 ]
 
 self.addEventListener('install', function(event) {
@@ -12,5 +28,32 @@ self.addEventListener('install', function(event) {
             return cache.addAll(cacheThese);
         })
     );
+
+});
+
+
+self.addEventListener('fetch', function(event) {
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                console.log('found ', event.target);
+                return response;
+            } else {
+                console.log('could not find that...');
+                return fetch(event.request)
+                    .then(function(response) {
+                        const cloneOne = response.clone()
+                        caches.open('app-v1').then(function(cache) {
+                            cache.put(event.request, cloneOne);
+                        })
+                        return response;
+                    })
+                    .catch(function(err) {
+                        console.log(err);
+                    })
+            }
+            // return response || fetch(event.request);
+        }));
 
 });
